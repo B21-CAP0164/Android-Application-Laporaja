b659472009d5046089a0e4b0314d42a3bcf0cce7
dendi6 <mdendi6@gmail.com>
2021-06-07 09:34:49 +0700

    english resources

--- app/src/main/java/com/bangkit/laporaja/views/setting/SettingsFragment.kt
+++ app/src/main/java/com/bangkit/laporaja/views/setting/SettingsFragment.kt
@@ -1,7 +1,9 @@
 package com.bangkit.laporaja.views.setting
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.preference.PreferenceFragmentCompat
+import android.provider.Settings
+import androidx.preference.*
 import com.bangkit.laporaja.MainActivity
 import com.bangkit.laporaja.R
 
@@ -10,9 +12,10 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         currentActivity = activity as MainActivity
-
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
-        currentActivity.removeBottomBar()
+        val mIntent = Intent(Settings.ACTION_LOCALE_SETTINGS)
+        startActivity(mIntent)
+
     }
 }
